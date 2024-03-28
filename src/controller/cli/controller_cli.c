b724ef136aa3c365e75a01e348524649c2923466
Ronin Shawanda <roninsha@arm-stage1.surgut.21-school.ru>
2024-03-28 12:58:30 +0500

    Clang-format -i

--- src/controller/cli/controller_cli.c
+++ src/controller/cli/controller_cli.c
@@ -1,13 +1,12 @@
-#include <pthread.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <malloc.h>
-#include <locale.h>
-#include <stdint.h>
-
-#include <stdio.h>
-
 #include "controller_cli.h"
+
+#include <locale.h>
+#include <malloc.h>
+#include <pthread.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
 
 struct data {
   pthread_mutex_t mutex_lock;
@@ -15,7 +14,7 @@ struct data {
 };
 
 int main() {
-  int code = 0;  
+  int code = 0;
   /* Все, что мы хотим отправить в поток, складываем в структуру,
   потому как операнд у функции создания потока только один */
   struct data data;
@@ -27,14 +26,15 @@ int main() {
   //инициализация исключающей блокировки
   pthread_mutex_init(&data.mutex_lock, NULL);
 
-  pthread_t pthread0; /* идентификатор потока */
+  pthread_t pthread0;  /* идентификатор потока */
   pthread_attr_t attr; /* аттрибуты потока */
 
   /* получаем дефолтные значения атрибутов */
   pthread_attr_init(&attr);
 
   /* создаем новый поток */
-  code = (pthread_create(&pthread0, &attr, controller_loop, (void *)&data) != 0);
+  code =
+      (pthread_create(&pthread0, &attr, controller_loop, (void *)&data) != 0);
 
   /* "Процесс завершается двумя путями - вызовом pthread_exit() или
    завершением потоковой функции. В случае, если процесс неотсоединен, то
@@ -43,49 +43,39 @@ int main() {
    освобождаются по ее завершению."  */
 
   /* ждем завершения исполнения потока */
-  // ret_value = pthread_join(pthread0, NULL);
+  // pthread_join(pthread0, NULL);
 
   // переводим в отсоединенный режим
   // pthread_detach(pthread0);
 
-  while (value2)
-  {
+  while (value2) {
     printf("Main: %d\n", value2--);
     sleep(1);
   }
 
   //Удаляем исключающую блокировку
   pthread_mutex_destroy(&data.mutex_lock);
-
-  // init ncurses
-  // setlocale(LC_ALL, "");
-  // initscr();
-
-  // main API logic
-  // controller_loop();
-
-  // end ncurses
-  // endwin();
+  
+  // sleep(1);
+  (void) pthread_join(pthread0, NULL);
 
   return code;
 }
 
-void * controller_loop(void *data) {
-    // TODO Uptade_cerent_state
-    // TODO user_input
-
+void *controller_loop(void *data) {
   struct data *data0 = (struct data *)data;
   int num = data0->value1;
-                       
-  while (num)
-  {
-    /* No other thread is going to join() this one - прикольно, самоотсоединение:*/
+
+  while (num) {
+    /* No other thread is going to join() this one - прикольно,
+     * самоотсоединение:*/
     pthread_detach(pthread_self());
 
     // устанавливаем блокировку
     pthread_mutex_lock(&data0->mutex_lock);
 
-    printf("Controller: %d\n", num--); // Операции ввода-вывода, изменение переменных, памяти.
+    printf("Controller: %d\n",
+           num--);  // Операции ввода-вывода, изменение переменных, памяти.
 
     // снимаем блокировку
     pthread_mutex_unlock(&data0->mutex_lock);
