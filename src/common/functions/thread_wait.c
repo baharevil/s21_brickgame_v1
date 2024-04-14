92fbbdc0c09e25daa6a355b27cc51c6133a51a89
Ladonna Harclay <ladonnah@su-f1.surgut.21-school.ru>
2024-04-14 19:20:37 +0500

    thread_wait() sec+nsec, code fix

--- src/common/functions/thread_wait.c
+++ src/common/functions/thread_wait.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <errno.h>
 #include <pthread.h>
 #include <sys/time.h>
 
@@ -8,27 +9,24 @@ int thread_wait(int msec) {
   pthread_cond_t condition;
   struct timespec how_much = {0};
 
-  code = pthread_mutex_init(&mutex, NULL);
-  if (!code) code = pthread_cond_init(&condition, NULL);
+  code = (msec == 0) * EINVAL;
 
+  if (!code) code = pthread_mutex_init(&mutex, NULL);
+  if (!code) code = pthread_cond_init(&condition, NULL);
   if (!code) code = !timespec_get(&how_much, TIME_UTC);
 
   if (!code) {
-    long sec = msec / 100;
-    msec -= sec * 100;
-    long nsec = msec * 10000;
+    long sec = msec / 1000;
+    msec -= sec * 1000;
+    long nsec = msec * 1000000;
     how_much.tv_sec += sec;
     how_much.tv_nsec += nsec;
   }
-  
-  if (!code) {
-    // pthread_mutex_lock(&mutex);
-    code = pthread_cond_timedwait(&condition, &mutex, &how_much);
-    // pthread_mutex_unlock(&mutex);
-  }
+  // Сон в ожидании
+  if (!code) code = pthread_cond_timedwait(&condition, &mutex, &how_much);
 
-  if (!code) code = pthread_mutex_destroy(&mutex);
-  if (!code) code = pthread_cond_destroy(&condition);
+  if (!code || code == ETIMEDOUT) code = pthread_mutex_destroy(&mutex);
+  if (!code || code == ETIMEDOUT) code = pthread_cond_destroy(&condition);
 
   return code;
 }
