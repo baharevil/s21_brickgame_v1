f4023017ae6fba3ba23f64c738b6c05cfb91d578
Igor Baharev <baharev.il@gmail.com>
2024-05-08 20:49:12 +0500

    Some thinks about render speed

--- src/gui/cli/functions/gui_cli_render.c
+++ src/gui/cli/functions/gui_cli_render.c
@@ -7,19 +7,36 @@
 void gui_cli_render(game_windows_t *windows, game_info_t *game_info) {
   if (!windows) return;
 
-  int term_x = 0, term_y = 0;
+  static int stored_x = 0, stored_y = 0, stored_hs = 0, stored_s = 0, stored_l = 0;
+  int term_x = 0, term_y = 0, resized = 0;
   term_size(&term_y, &term_x);
   
-  render_field(&windows->game_win, game_info->field, field_higth, field_windth,
-               NULL);
+  resized = (stored_x != term_x || stored_y != term_y);
+
+  render_field(&windows->game_win, game_info->field, field_height, field_width, NULL);
   // !Bug with next btick  
   render_field(&windows->stats_windows.next_win.window, game_info->next,
                brick_higth, brick_windth,
                windows->stats_windows.next_win.label);
 
-  render_score(&windows->stats_windows.max_win, game_info->high_score);
-  render_score(&windows->stats_windows.score_win, game_info->score);
-  render_score(&windows->stats_windows.level_win, game_info->level);
+  if (resized || stored_hs != game_info->high_score) {
+    render_score(&windows->stats_windows.max_win, game_info->high_score);
+    stored_hs = game_info->high_score;
+  }
+  if (resized || stored_s != game_info->score) {
+    render_score(&windows->stats_windows.score_win, game_info->score);
+    stored_s = game_info->score;
+  }
+  if (resized || stored_l != game_info->level) {
+    render_score(&windows->stats_windows.level_win, game_info->level);
+    stored_l = game_info->level;
+  }
+  // Тут не попал
+  if (resized) {
+    box(windows->main_win.win, 0, 0);
+    box(windows->game_win.win, 0, 0);
+    box(windows->stat_win.win, 0, 0);
+  }
 
   doupdate();
 }
@@ -29,13 +46,14 @@ int render_field(win_t *windows, int **field, int higth, int width,
   if (!windows || !field) return EFAULT;
 
   // clear
-  wclear(windows->win);
+  // wclear(windows->win);
   box(windows->win, 0, 0);
 
   // render
   for (int i = 0; i < higth; i++) {
     for (int j = 0; j < width; j++)
       if (field[i][j]) mvwaddstr(windows->win, 1 + i, 1 + 2 * j, "[]");
+      else mvwaddstr(windows->win, 1 + i, 1 + 2 * j, "  ");
   }
   if (label) mvwaddstr(windows->win, 1, 1, label);
 
@@ -48,7 +66,7 @@ int render_score(support_win_t *supp_win, int score) {
   if (!supp_win) return EFAULT;
   
   // clear 
-  wclear(supp_win->window.win);
+  // wclear(supp_win->window.win);
   box(supp_win->window.win, 0, 0);
 
   // render
